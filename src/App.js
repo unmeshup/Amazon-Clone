271ba5ebb2ca9ae4e73b65aed0e1d485425527ae
Unmesh Pawar <unplfc@gmail.com>
2023-05-03 20:13:06 +0530

    Firebase setup and react router dom setup completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home";
+import Checkout from "./Checkout";
+import React, { useEffect }  from "react";
+import Login from "./Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+      <Route exact path='/login' element={
+          <React.Fragment>
+           <Header />
+            <Login />
+          
+          </React.Fragment>
+        }/>
+        
+        <Route exact path='/' element={
+          <React.Fragment>
+            <Header />
+            <Home />
+            
+          </React.Fragment>
+        }/>
+        <Route exact path='/checkout'  element={
+          <React.Fragment>
+            <Header />
+            <Checkout />
+          </React.Fragment>
+        } />
+      </Routes>
+    </Router>
   );
 }
 
