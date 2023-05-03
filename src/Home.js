92f441152212e304bfaa7a661955c65e22a541f3
Unmesh Pawar <unplfc@gmail.com>
2023-05-03 20:16:37 +0530

    Navigation bar and banner done

---
+++ src/Home.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt=""
+        />
+
+        <div className="home__row">
+        <Product
+            id="1235"
+            title="Samsung Galaxy M12 (Blue,4GB RAM, 64GB Storage) 6000 mAh with 8nm Processor | True 48 MP Quad Camera | 90Hz Refresh Rate"
+            price={9499.0}
+            image="https://m.media-amazon.com/images/I/71r69Y7BSeL._SL1500_.jpg"
+            rating={4}
+          />
+           <Product
+            id="5646"
+            title="Mi 80 cm (32 inches) HD Ready Android Smart LED TV 4A PRO | L32M5-AL (Black)"
+            price={14999.0}
+            image="https://m.media-amazon.com/images/I/71Z+l05eSIS._SL1188_.jpg"
+            rating={2}
+          />
+        </div>
+
+        <div className="home__row">
+        <Product
+            id="4569"
+            title="
+            DecorDen Zigzag Floating Wall Mount Wooden Display Storage Organizer Shelf for Wall Decoration (Brown)"
+            price={600.0}
+            image="https://m.media-amazon.com/images/I/51Ey7WCBV8S._SL1400_.jpg"
+            rating={4}
+          />
+          <Product
+            id="23445930"
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+            price={98.99}
+            rating={5}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+          />
+          <Product
+            id="3254354345"
+            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+            price={598.99}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+          />
+        </div>
+
+        <div className="home__row">
+          <Product
+            id="90829332"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+            price={1094.98}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
